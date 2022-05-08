94b19ccd3cbd9cf08200c81e5410838a085a85d2
chugaev101 <chugaev101@gmail.com>
2022-05-08 08:46:32 +0300

    Implements the logic of the show more button.

--- src/model/movie-model.js
+++ src/model/movie-model.js
@@ -2,9 +2,9 @@ import { generateMovie } from '../mock/movie.js';
 import { getRandomInteger } from '../utils.js';
 
 export default class MovieModel {
-  #movies = Array.from({ length: getRandomInteger(5, 22) }, generateMovie);
+  #movies = Array.from({ length: getRandomInteger(0, 22) }, generateMovie);
 
   get movies () {
-    return this.#movies;
+    return this.#movies; 
   }
 }
